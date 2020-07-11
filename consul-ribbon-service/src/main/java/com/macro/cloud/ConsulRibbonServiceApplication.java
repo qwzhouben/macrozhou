a35b9652b1c027d50701f867278b6456f2af8fd0
周奔 <zhou_ben@gzhc365.com>
2020-07-11 08:35:37 +0800

    consul注册中心和配置中心

---
+++ consul-ribbon-service/src/main/java/com/macro/cloud/ConsulRibbonServiceApplication.java
@@ -0,0 +1,19 @@
+package com.macro.cloud;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+
+/**
+ * @DESC:
+ * @author: zhouben
+ * @date: 2020/7/9 0009 14:21
+ */
+@EnableDiscoveryClient
+@SpringBootApplication
+public class ConsulRibbonServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ConsulRibbonServiceApplication.class, args);
+    }
+}
